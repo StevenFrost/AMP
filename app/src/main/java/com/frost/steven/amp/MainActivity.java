e4ece2c416625bf20677d6bfa0e060c944e394cf
Steven Frost <steve.frost.private@gmail.com>
2015-11-02 19:29:34 +0000

    Android Studio project files

---
+++ app/src/main/java/com/frost/steven/amp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.frost.steven.amp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
